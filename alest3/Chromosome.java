6633483ca043daaa70fd184c2b727079fac08577
leonardo.vargas <leonardo.vargas@acertsis.com.br>
2022-11-09 22:07:21 -0300

    Remove população e otimiza mutação

--- alest3/Chromosome.java
+++ alest3/Chromosome.java
@@ -4,40 +4,46 @@ import java.util.ArrayList;
 
 public class Chromosome {
 
-    public final ArrayList<Integer> sequence;
-    public double fitness;
+    private final ArrayList<City> cities;
+    private double distance;
 
-    Chromosome(ArrayList<Integer> sequence) {
-        this.sequence = sequence;
-        this.fitness = 0;
+    Chromosome(ArrayList<City> cities) {
+        this.cities = cities;
     }
 
-    double getFitness() {
-        return fitness;
+    Chromosome(ArrayList<City> cities, double distance) {
+        this.cities = cities;
+        this.distance = distance;
     }
 
-    double distance(final City a, final City b) {
+    public double getDistance() {
+        return distance;
+    }
+
+    public Chromosome copy() {
+        return new Chromosome(new ArrayList<>(cities), distance);
+    }
+
+    private double distance(final City a, final City b) {
         final var x = Math.pow(b.coordinates[0] - a.coordinates[0], 2);
         final var y = Math.pow(b.coordinates[1] - a.coordinates[1], 2);
         return Math.sqrt(x + y);
     }
 
-    public void calculateFitness(final ArrayList<City> cities) {
+    public double calculateFitness() {
         double totalDistance = 0;
-        for (int i = 0; i < sequence.size() - 1; i++) {
-            totalDistance += distance(cities.get(sequence.get(i)), cities.get(sequence.get(i + 1)));
-        }
-        totalDistance += distance(cities.get(sequence.get(sequence.size() - 1)), cities.get(sequence.get(0)));
-        fitness = 1 / totalDistance;
+        for (int i = 0; i < cities.size() - 1; i++) totalDistance += distance(cities.get(i), cities.get(i + 1));
+        this.distance = totalDistance + distance(cities.get(cities.size() - 1), cities.get(0));
+        return distance;
     }
 
-    void mutate(double chance) {
-        if (Math.random() < chance) {
-            final var index1 = (int) (Math.random() * sequence.size());
-            final var index2 = (int) (Math.random() * sequence.size());
-            final var temp = sequence.get(index1);
-            sequence.set(index1, sequence.get(index2));
-            sequence.set(index2, temp);
-        }
+    public void mutate() {
+        final var a = (int) (Math.random() * cities.size());
+        var b = (int) (Math.random() * cities.size());
+        while (a == b) b = (int) (Math.random() * cities.size());
+        final var start = Math.min(a, b);
+        final var end = Math.max(a, b);
+        final var copy = new ArrayList<>(cities.subList(start, end));
+        for (int i = 0; i < copy.size(); i++) cities.set(start + i, copy.get(copy.size() - i - 1));
     }
 }
--- alest3/TravellingSalesmanApplication.java
+++ alest3/TravellingSalesmanApplication.java
@@ -2,9 +2,9 @@ package alest3;
 
 import java.io.IOException;
 
-public class TravellingSalesman {
+public class TravellingSalesmanApplication {
 
     public static void main(String[] args) throws IOException {
-        new GeneticAlgorithm("alest3/data.txt").run(1000000,100, 0.1);
+        new GeneticAlgorithm("alest3/data.txt").run(100000000);
     }
 }
